9993a66e24263d6f7979de0a0be83963dd22b5c4
Anant Pandey <pandeyanant366@gmail.com>
2024-07-15 00:23:41 +0530

    Defined model schema and db connection
    Models related details like schema etc are in folders named models, seeds which are not committed.

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/api', (req, res) => {
   res.json({ message: "Hello from Express!" });
 });
+// app.get('/makeCampground', async (req, res) => {
+//   const camp = new Campground({title:'My Backyard', description:'Cheap Camping'});
+//   await camp.save();
+//   res.send(camp)
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
